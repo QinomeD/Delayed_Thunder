fda5361bfb84424b3e9a4aa5bfc3e44c4968c49c
BuildTools <unconfigured@null.spigotmc.org>
2024-07-23 19:26:14 -0700

    Cleaning Up

--- src/main/java/net/jewwis/betterlightning/soundevent/SoundEventHandler.java
+++ src/main/java/net/jewwis/betterlightning/soundevent/SoundEventHandler.java
@@ -1,21 +1,17 @@
 package net.jewwis.betterlightning.soundevent;
 
 import net.jewwis.betterlightning.BetterLightning;
+import net.jewwis.betterlightning.Config;
 import net.jewwis.betterlightning.ticktimer.TickEventHandler;
 import net.jewwis.betterlightning.ticktimer.TickTimer;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.sounds.Sound;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -27,45 +23,42 @@ import static net.minecraftforge.client.ForgeHooksClient.playSound;
 public class SoundEventHandler {
 
     @OnlyIn(Dist.CLIENT)
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOW)
     public static void onPlaySound(PlaySoundEvent event){
 
         if (event.getName().equalsIgnoreCase("entity.lightning_bolt.thunder")){
             float x2 = (float) Objects.requireNonNull(event.getSound()).getX();
             float y2 = (float) Objects.requireNonNull(event.getSound()).getY();
             float z2 = (float) Objects.requireNonNull(event.getSound()).getZ();
-            Player player = Minecraft.getInstance().player;
 
+            Player player = Minecraft.getInstance().player;
             if (player != null){
-                BlockPos lightningPos = new BlockPos((int)event.getSound().getX(), (int)event.getSound().getY(), (int)event.getSound().getZ());
-                BlockPos playerPos = player.blockPosition();
                 float x1 = (float) player.getX();
                 float y1 = (float) player.getY();
                 float z1 = (float) player.getZ();
                 int a = getDistance(x1, y1, z1, x2, y2, z2);
-                player.sendSystemMessage(Component.literal("Position Thunder: " + lightningPos));
-                player.sendSystemMessage(Component.literal("Position Player: " + playerPos));
                 player.sendSystemMessage(Component.literal("Distance: " + a));
+                event.setSound(null);
 
-                if (a >= 60 && a < 80) {
-                    event.setSound(null);
-                    TickEventHandler.addTimer(new TickTimer(60, () -> {
-                        player.sendSystemMessage(Component.literal("3 Seconds Delay"));
-                        player.playSound(ModSounds.SHORTTHUNDER.get());
+                if (a >= Config.distanceShort && a < Config.distanceMedium) {
+                    TickEventHandler.addTimer(new TickTimer(Config.timeShort, () -> {
+                        System.out.println("2.25 Seconds Delay");
+                        player.playSound(ModSounds.SHORTTHUNDER.get(), 0.8f, 1.0f);
                     }));
-                } else if (a >= 80 && a < 115) {
-                    event.setSound(null);
-                    TickEventHandler.addTimer(new TickTimer(100, () -> {
-                        player.sendSystemMessage(Component.literal("5 Seconds Delay"));
-                        player.playSound(ModSounds.MEDIUMTHUNDER.get());
+                } else if (a >= Config.distanceMedium && a < Config.distanceFar) {
+                    TickEventHandler.addTimer(new TickTimer(Config.timeMedium, () -> {
+                        System.out.println("4.5 Seconds Delay");
+                        player.playSound(ModSounds.MEDIUMTHUNDER.get(), 2.0f, 1.0f);
                     }));
-                } else if (a >= 115) {
-                    event.setSound(null);
-                    TickEventHandler.addTimer(new TickTimer(160, () -> {
-                        player.sendSystemMessage(Component.literal("8 Seconds Delay"));
-                        player.playSound(ModSounds.FARTHUNDER.get());
+                } else if (a >= Config.distanceFar) {
+                    TickEventHandler.addTimer(new TickTimer(Config.timeLong, () -> {
+                        System.out.println("7.5 Seconds Delay");
+                        player.playSound(ModSounds.FARTHUNDER.get(), 1.0f, 1.0f);
                     }));
+                } else {
+                    player.playSound(ModSounds.NORMALTHUNDER.get(), 1.0f, 1.0f);
                 }
+
             }
         }
 
@@ -92,12 +85,3 @@ public class SoundEventHandler {
         return sqrtRoot;
     }
 }
-
-
-
-//String soundName = event.getName();
-//System.out.println("Sound: " + soundName);
-//Sound: entity.lightning_bolt.thunder
-//Sound: entity.lightning_bolt.impact
-//<60 <80 <100
-//10T 20T 40T
